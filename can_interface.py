2c405ee2d21d3c04144c181b99f202a6c37b3a80
Cole Barach <colebarach@tutanota.com>
2023-02-05 14:20:57 -0500

    Closed CAN Channel 1

--- can_interface.py
+++ can_interface.py
@@ -80,7 +80,7 @@ def Setup(database):
             import lib_innomaker_win
             library = lib_innomaker_win.Main(database, messageHandler=HandleMessage, timingFunction=SetTimeouts, timingPeriod=config.CAN_TIME_PERIOD)
             library.OpenChannel(config.CAN_BITRATE, 0)
-            library.OpenChannel(config.CAN_BITRATE, 1)
+            # library.OpenChannel(config.CAN_BITRATE, 1)
             return library
 
         if(sys.platform == "linux"):
